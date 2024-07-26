390d126a537da27438ae41087bacda16271f2c1c
Nayeli-Urrutia <160196354+Nayeli-Urrutia@users.noreply.github.com>
2024-07-25 21:01:04 -0600

    Se termino los ejercicios

--- src/main/java/umg/Progra2/Main.java
+++ src/main/java/umg/Progra2/Main.java
@@ -1,5 +1,5 @@
 package umg.Progra2;
-
+import java.util.Scanner;
 import umg.Progra2.grupo1.ejercicios1;
 import umg.Progra2.grupo2.ejerccio2;
 import umg.Progra2.grupo3.ejercicio3;
@@ -7,12 +7,44 @@ import umg.Progra2.grupo3.ejercicio3;
 public class Main {
     public static void main(String[] args) {
 
-        ejercicios1 ej = new ejercicios1();
 
-        ejerccio2 ej2 = new ejerccio2();
+            Scanner sc = new Scanner(System.in);
+            int opcion;
+            ejercicios1 ej1;
+            ejerccio2 ej2;
+            ejercicio3 ej3;
 
-        ejercicio3 ej3 = new ejercicio3();
+            do {
+                System.out.println("---------------------------------");
+                System.out.println("---MENÚ DE EJERCICIOS EN CLASE---");
+                System.out.println("---------------------------------");
+                System.out.println("1. Ejercicios1");
+                System.out.println("2. Ejercicios2");
+                System.out.println("3. Ejercicios3");
+                System.out.println("4. Salir del Programa");
+                System.out.print("Ingrese el número de opción: ");
+                opcion = sc.nextInt();
+                switch (opcion) {
+                    case 1:
+                        ej1 = new ejercicios1();
+                        break;
+                    case 2:
+                        ej2 = new ejerccio2();
+                        break;
+                    case 3:
+                        ej3 = new ejercicio3();
+                        break;
+                    case 4:
+                        System.out.println("Saliendo del programa...");
+                        break;
+                    default:
+                        System.out.println("Opción no válida. Por favor, ingrese un número válido.");
+                        break;
+                }
+            } while (opcion != 4);
 
-
+            sc.close();
+        }
     }
-}
+
+
--- src/main/java/umg/Progra2/grupo3/ejercicio3.java
+++ src/main/java/umg/Progra2/grupo3/ejercicio3.java
@@ -7,11 +7,44 @@ public class ejercicio3 {
     public ejercicio3() {
         Scanner sns = new Scanner(System.in);
 
-            System.out.print("Ingrese la longitud del prisma: ");
+        //Ejercicio 3.1
+        System.out.println("VOLUMEN DE UN PRISMA RETANGULAR");
+        System.out.print("Ingrese el área base del prisma: ");
 
-            double longitud = sns.nextDouble();
+        double base = sns.nextDouble();
+        System.out.print("Ingrese la altura del prisma: ");
+        double altura = sns.nextDouble();
+        double volumen = base * altura;
+        String Final1 = String.format("%.2f", volumen);
+        System.out.println("El volumen del prisma rectangular es: " + Final1);
 
+        //Enter para continuar
+        sns.nextLine();
+        sns.nextLine();
 
+        //Ejercicio 3.2
+        System.out.println("ÁREA DE UN PARALELOGRAMO");
+        System.out.println("Ingresar la altura del Paralelogramo: ");
+        double alturaParalelo = sns.nextDouble();
+        System.out.println("Ingresar la base del Paralelogramo: ");
+        double baseParalelo = sns.nextDouble();
+        double AreaParalelo = baseParalelo * alturaParalelo;
+        String Final2 = String.format("%.2f", AreaParalelo);
+        System.out.println("El área del Paralelogramo es: " + Final2);
+
+        //Enter para continuar
+        sns.nextLine();
+        sns.nextLine();
+
+        // Ejercicio 3.3
+        System.out.println("VOLUMEN DE UN CILINDRO");
+        System.out.print("Ingrese el radio del cilindro: ");
+        double radio = sns.nextDouble();
+        System.out.print("Ingrese la altura del cilindro: ");
+        double alturaCilindro = sns.nextDouble();
+        double volumenCilindro = Math.PI * Math.pow(radio, 2) * alturaCilindro;
+        String Final3 = String.format("%.2f", volumenCilindro);
+        System.out.println("El volumen del cilindro es: " + Final3);
 
 
 
